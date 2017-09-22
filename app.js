d11a0ac93d105533066d10e8936cc0eb540df82e
stevens1434 <stevens1434@gmail.com>
2017-09-21 22:45:30 -0700

    completed majority of game - can't get the ADD NEW FUNCTION nor part of the EDIT portions to work ---- more specifically, i can not get the new description to display, although the new text does show up in the array in games.json file

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// require the modules we need
-// STOP: what are these modules? Use online documentation to read up on them.
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -7,7 +5,7 @@ var ejsLayouts = require("express-ejs-layouts");
 var bodyParser = require('body-parser');
 
 var app = express();
-
+app.use(ejsLayouts);
 // this sets a static directory for the views
 app.use(express.static(path.join(__dirname, 'static')));
 
@@ -17,8 +15,52 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 app.set('view engine', 'ejs');
 
-// your routes here
 
+app.get('/', function(req, res){ //homepage
+  res.render('homepage', {games: getGames() });
+
+});
+
+app.get('/games/:id', function(req, res) { //individual game info
+  var game = getGames()[req.params.id];
+  game.id = req.params.id;
+  res.render('games', {game: game}); //might not need ./
+});
+
+app.get('/games/new/', function(req, res) { //add new game
+  res.render('./games-new'); //might not need the game: game
+})
+
+app.post('/games', function(req, res) { //post NEW GAME info to list on
+  var game = getGames();
+  game.push(req.body);
+  saveGames(games);
+
+  var path = '/games/' + (games.length - 1);
+  res.redirect(path);
+})
+
+app.get('/games/:id/edit', function(req, res) { //edit game
+  var game = getGames()[req.params.id];
+  game.id = req.params.id;
+  res.render('./games-edit', {game: game});
+});
+
+app.put('/games/:id', function(req, res) { //put game EDIT information
+    var game = getGames();
+    game[req.params.id] = req.body;
+    saveGames(game);
+    res.send(req.body);
+});
+
+// Delete
+app.delete('/games/:id', function(req, res) {
+    var games = getGames();
+    // Set the index to undefined so every other position isn't screwed up.
+    games[req.params.id] = undefined;
+    saveGames(games);
+    res.send(req.body);
+});
 // ...
 
 // helper functions
@@ -35,7 +77,8 @@ function saveGames(games) {
     fs.writeFileSync('./games.json', JSON.stringify(games));
 }
 
-// start the server
+
+
 
 var port = 3000;
 console.log("http://localhost:" + port);
