cd41ccffb9b7f98f08d63c966590918ba2b65437
tbricca <tbricca@gmail.com>
2017-09-22 09:34:06 -0700

    was not able to get it working

--- static/js/main.js
+++ static/js/main.js
@@ -4,9 +4,33 @@ console.log("JS good to go, sir!");
 // at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
 // appropriate URL
 
-// code here ...
+$('put-form').on('submit',function(e) {
+	e.preventDefault();
+	var element=$(this);
+	var url=element.attr('action');
+	var formData = element.serialize();
+	$.ajax({
+		mehtod: 'PU'
+		url: url,
+		data: formData
+	}). done(function)(data) {
+
+		window.location = url;
+	});
+});
 
 // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
 // to the appropriate URL
 
-// code here ...
+$('.delete-link').on('click', function(e) {
+	e.preventDefault();
+	var element = $(this);
+	var url = element.attr('href')
+	$.ajax({
+		method: 'DELETE',
+		url: uel
+	}).done(function(data) {
+		window.location = '/';
+	});
+});
+
