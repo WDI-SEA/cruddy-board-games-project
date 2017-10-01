3ec60209176b2e223bb9e6ec416d147239d951db
ElenaOl <pravutiner@gmail.com>
2017-10-01 16:18:02 -0700

    by Elena

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,44 @@
 console.log("JS good to go, sir!");
 
 
-//$.get('/')
+
 
 // listen for a "submit" event for the edit page. the edit form will live
-// at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
-// appropriate URL
+$('[type=button]').on('click', function(e) {
+    //e.preventDefault();
+    var updateUrl = $(this).attr('editUrl');
+    //will take form data and put in article data
+    var body = $('#body').serialize();
+    // at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
+    // appropriate URL
+    $.ajax({
+      method: 'PUT',
+      url: updateUrl,
+      data: body
+    }).done(function(data) {
+      // get data returned from the PUT route
+      console.log(data);
+    });
+  });
 
-// code here ...
+
+
+
+
+
 
 // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
 // to the appropriate URL
 
-// code here ...
+$('.delete-game').on('click', function(e){
+    //e.preventDefault();
+    var deleteUrl = $(this).attr('deleteUrl');
+    console.log('deleting');
+    $.ajax({
+      method:'DELETE',
+      url: deleteUrl
+    }).done(function(data){
+      console.log(data);
+    });
+});
+
